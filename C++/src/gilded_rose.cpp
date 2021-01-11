30864b45eef7ec582a0d321d028f8b3d99b2ad98
Van Lembergen, Roeland <roeland.vanlembergen@barco.com>
2021-01-11 23:34:12 +0100

    Solution: isolate behavior of "Sulfuras". No unit tests fail, so nothing to be done for this item type.

--- C++/src/gilded_rose.cpp
+++ C++/src/gilded_rose.cpp
@@ -48,6 +48,10 @@ void GildedRose::tick()
         return;
     }
 
+    if (_name == kItemSulfuras) {
+        return;
+    }
+
     if (_name != kItemAgedBrie && _name != kItemBackstagePasses) {
         if (_quality > 0) {
             if (_name != kItemSulfuras) {
